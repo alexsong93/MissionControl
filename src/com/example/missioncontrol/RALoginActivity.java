8c9e506232c4c5afd165c749819f503f55727ec4
Minkwon Lee <ml241@duke.edu>
2015-05-08 05:09:30 -0400

    hiding keyboard

--- src/com/example/missioncontrol/RALoginActivity.java
+++ src/com/example/missioncontrol/RALoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.missioncontrol;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 public class RALoginActivity extends ActionBarActivity {
@@ -43,4 +46,12 @@ public class RALoginActivity extends ActionBarActivity {
 		Intent intent = new Intent(this, RAregisterActivity.class);
 		startActivity(intent);
 	}
+	
+	@Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.
+                                                        INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+    }
 }
