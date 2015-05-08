de26e790d2895268dbec4246086c8e4257434d80
Minkwon Lee <ml241@duke.edu>
2015-05-08 00:54:35 -0400

    implementing ra login activity and xml

--- src/com/example/missioncontrol/RALoginActivity.java
+++ src/com/example/missioncontrol/RALoginActivity.java
@@ -1,5 +1,11 @@
 package com.example.missioncontrol;
-
-public class RALoginActivity {
-
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+public class RALoginActivity extends ActionBarActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ralogin);
+	}
 }
