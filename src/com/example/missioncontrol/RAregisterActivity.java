8ea62a76151aa245b0b223374f98d65f0710b955
Minkwon Lee <ml241@duke.edu>
2015-05-08 00:36:12 -0400

    implementing ra register

--- src/com/example/missioncontrol/RAregisterActivity.java
+++ src/com/example/missioncontrol/RAregisterActivity.java
@@ -1,7 +1,27 @@
 package com.example.missioncontrol;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RAregisterActivity extends ActionBarActivity {
 	
+	EditText nameInput;
+	EditText emailInput;
+	EditText pwInput;
+	Button regisBtn;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_raregister);
+		
+		nameInput = (EditText) findViewById(R.id.editText1);
+		emailInput = (EditText) findViewById(R.id.editText2);
+		pwInput = (EditText) findViewById(R.id.editText3);
+		regisBtn = (Button) findViewById(R.id.button1);
+		
+	}
 }
